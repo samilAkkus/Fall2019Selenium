0480862b8247a3e0ccedf91a63d5f304d04e985d
samilAkkus <samilakgus15@email.com>
2020-03-01 10:54:19 -0800

    open browser, test and close

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         //to start selenium script we need:
         //setup webDriver(browser driver) and create webDriver object
@@ -19,5 +19,28 @@ public class BasicNavigation {
         //in selenium, everything starts from WebDriver interface
 
         driver.get("http://google.com"); //essential to open a website
+
+        Thread.sleep(2000); //wait 3 seconds,
+        // it throws exception,
+        // i added "throws" keyword in main method
+
+        String title = driver.getTitle(); //returns <title>SomeTitle</title>
+        //If the page is Google, it returns title(Google)
+
+        String expectedTitle = "Google";
+
+        System.out.println("title = " + title);
+
+        if(title.equals(expectedTitle)){
+
+            System.out.println("TEST PASSED!!");
+        }else {
+
+            System.out.println("TEST FAILED!!");
+        }
+
+        driver.close(); // to close the browser
+
+
     }
 }
